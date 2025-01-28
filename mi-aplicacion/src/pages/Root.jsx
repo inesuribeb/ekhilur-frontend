de0ee11536683c9ebdfe180ed7d567eebf64dbcc
Izorrai <latxiondo.jon@gmail.com>
2025-01-28 09:25:28 +0100

    navbar creado

--- mi-aplicacion/src/pages/Root.jsx
+++ mi-aplicacion/src/pages/Root.jsx
@@ -2,13 +2,17 @@ import { Outlet } from "react-router-dom";
 import "./Root.css";
 import Header from "../components/header/Header"
 import Footer from "../components/footer/Footer"
+import MobileNavbar from "../components/navbar/MobileNavbar";
+import DesktopNavbar from "../components/navbar/DesktopNavbar";
 
 function Root() {
     return (
         <>
-            <Header />
+            <DesktopNavbar />
+             <Header />
             <Outlet />
             <Footer />
+            <MobileNavbar />
         </>
     );
 }
