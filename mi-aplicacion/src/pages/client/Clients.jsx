376c369f4b2e47b12242f4bf783cf200d738ce26
Izorrai <latxiondo.jon@gmail.com>
2025-02-04 18:03:15 +0100

    conflictos resueltos

--- mi-aplicacion/src/pages/client/Clients.jsx
+++ mi-aplicacion/src/pages/client/Clients.jsx
@@ -32,6 +32,12 @@ ChartJS.register(
     Filler
 );
 
+const monthTranslations = {
+    1: 'Ene', 2: 'Feb', 3: 'Mar', 4: 'Abr', 
+    5: 'May', 6: 'Jun', 7: 'Jul', 8: 'Ago', 
+    9: 'Sep', 10: 'Oct', 11: 'Nov', 12: 'Dic'
+};
+
 
 function Clients() {
     const [data, setData] = useState(null);
